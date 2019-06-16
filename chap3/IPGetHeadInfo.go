ade834e89b5729e3a46a21521f526deb14f4cbff
Patrice PETIT <ppetitpatrice@gmail.com>
2019-06-16 22:15:13 +0200

    chap3: IP

---
+++ chap3/IPGetHeadInfo.go
@@ -0,0 +1,47 @@
+package chap3
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+func readFully(conn net.Conn) ([]byte, error) {
+	defer conn.Close()
+
+	result := bytes.NewBuffer(nil)
+	var buf [512]byte
+
+	for {
+		n, err := conn.Read(buf[0:])
+		result.Write(buf[0:n])
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+	}
+	return result.Bytes(), nil
+}
+
+func IPGetHeadInfo(args []string) {
+
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "Usage: %s host:port", "IPGetHeadInfo")
+		os.Exit(1)
+	}
+
+	service := args[0]
+
+	conn, err := net.Dial("tcp", service)
+	checkError(err, 2)
+
+	_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
+	checkError(err, 3)
+
+	result, err := readFully(conn)
+
+}
