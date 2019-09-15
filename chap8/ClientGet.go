5b5497159f32e983cc2b68b23f9a4fa50f17101b
Patrice PETIT <ppetitpatrice@gmail.com>
2019-09-15 20:12:06 +0200

    feature/http: client GET

--- chap8/ClientGet.go
+++ chap8/ClientGet.go
@@ -3,14 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
 	"os"
+	"strings"
 )
+
+func getCharset(response *http.Response) string {
+	contentType := response.Header.Get("Content-Type")
+
+	if contentType == "" {
+		return "utf-8"
+	}
+
+	idx := strings.Index(contentType, "charset=")
+	if idx == -1 {
+		return "utf-8"
+	}
+
+	chSet := strings.Trim(contentType[idx+8:], " ")
+	return strings.ToLower(chSet)
+
+}
 
 func ClientGet(args []string) {
 
-	if len(args) != 2 {
+	if len(args) < 1 {
 		fmt.Println("Usage: make run target=ClientGet host=http://host:port/page")
+		os.Exit(1)
 	}
 
 	url, err := url.Parse(args[0])
@@ -18,7 +38,7 @@ func ClientGet(args []string) {
 
 	client := &http.Client{}
 
-	request, err := http.NewRequest("HEAD", url.String(), nil)
+	request, err := http.NewRequest("GET", url.String(), nil)
 	checkError(err)
 	request.Header.Add("Accept-Charset", "uft-8;q=1, ISO-8859-1;q=0")
 
@@ -29,4 +49,14 @@ func ClientGet(args []string) {
 		fmt.Println(response.Status)
 		os.Exit(2)
 	}
+
+	fmt.Println("The response header is")
+	b, _ := httputil.DumpResponse(response, false)
+
+	fmt.Print(string(b))
+
+	chSet := getCharset(response)
+
+	fmt.Println(chSet)
+
 }
---
+++ chap8/ProxyGet.go
@@ -0,0 +1,5 @@
+package main
+
+func ProxyGet(args string[]) {
+
+}
