44c1c2dab9cf0e7a82ae5bcee9c0083d69751e71
Patrice PETIT <ppetitpatrice@gmail.com>
2019-09-19 21:23:12 +0200

    feature/http: proxy[DONE]

--- chap8/ProxyGet.go
+++ chap8/ProxyGet.go
@@ -17,18 +17,17 @@ func ProxyGet(args []string) {
 	}
 
 	proxyString := args[1]
-
 	proxyURL, err := url.Parse(proxyString)
 	checkError(err)
 
-	rawUrl := args[0]
+	rawURL := args[0]
 
-	url, err := url.Parse(rawUrl)
+	url, err := url.Parse(rawURL)
 	checkError(err)
 
 	transport := &http.Transport{Proxy: http.ProxyURL(proxyURL)}
 
-	// client := &http.Client{Transport: transport}
+	client := &http.Client{Transport: transport}
 
 	request, err := http.NewRequest("GET", url.String(), nil)
 
@@ -40,4 +39,26 @@ func ProxyGet(args []string) {
 	dump, _ := httputil.DumpRequest(request, false)
 
 	fmt.Println(string(dump))
+
+	response, err := client.Do(request)
+
+	if response.StatusCode != 200 {
+		fmt.Println(response.Status)
+		os.Exit(2)
+	}
+
+	fmt.Println(response.Status)
+
+	var buf [512]byte
+	reader := response.Body
+
+	for {
+		n, err := reader.Read(buf[0:])
+		if err != nil {
+			os.Exit(0)
+		}
+		fmt.Print(string(buf[0:n]))
+	}
+
+	os.Exit(0)
 }
